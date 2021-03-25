5b7a073e466b31521aaf7aca6e36b64e9949768d
dfblhmm <1449050328@qq.com>
2021-03-25 19:04:27 +0800

    .

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,17 @@
 import React, { Component, Fragment } from 'react'
-
+import { Row, Col } from 'antd'
+import logo from 'assets/images/logo.png'
 export default class HeaderContainer extends Component {
   render() {
     return (
       <Fragment>
-        头部
+        <Row>
+          <Col span={8}>
+            <img src={logo} alt=""/>
+          </Col>
+          <Col span={8}>col-8</Col>
+          <Col span={8}>col-8</Col>
+        </Row>
       </Fragment>
     )
   }
