62e75aa407a3b23d5c515fba839152cf7b85470f
dfblhmm <1449050328@qq.com>
2021-03-26 13:53:22 +0800

    .

--- src/components/IconFont/index.ts
+++ src/components/IconFont/index.ts
@@ -1,4 +1,4 @@
 import { createFromIconfontCN } from '@ant-design/icons'
 export default createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_2124451_dd8233u18o.js',
+  scriptUrl: '//at.alicdn.com/t/font_2124451_34y7wwgy4aw.js',
 })
--- src/components/Sider/index.tsx
+++ src/components/Sider/index.tsx
@@ -1,37 +1,45 @@
-import React, { Component, Fragment } from 'react'
-import { Menu } from 'antd'
+import { Component, Fragment } from 'react'
+import { Menu, Collapse } from 'antd'
 import './index.scss'
 // 获取IconFont字体图标
 import IconFont from 'components/IconFont'
-const { SubMenu } = Menu
+import { CaretRightOutlined } from '@ant-design/icons'
+const { Panel } = Collapse
 export default class SiderContainer extends Component {
   render() {
     return (
       <Fragment>
-        <Menu style={{ width: 224, marginTop: '10px' }} defaultSelectedKeys={['1']} mode="inline">
+        <Menu
+          style={{ width: 224, marginTop: '10px' }}
+          defaultSelectedKeys={['1']}
+          mode="inline"
+        >
           <Menu.Item key="1">发现音乐</Menu.Item>
           <Menu.Item key="2">视频</Menu.Item>
           <Menu.Item key="3">朋友</Menu.Item>
           <Menu.Item key="4">私人FM</Menu.Item>
           <li className="title">我的音乐</li>
-          <Menu.Item key="5" style={{fontWeight: 'normal', fontSize: '14px'}}>
-            <IconFont type="icon-cloud"/>我的音乐云盘
+          <Menu.Item key="5" style={{ fontWeight: 'normal', fontSize: '14px' }}>
+            <IconFont type="icon-cloud" />我的音乐云盘
           </Menu.Item>
-          <Menu.Item key="6" style={{fontWeight: 'normal', fontSize: '14px'}}>
-            <IconFont type="icon-radio"/>我的电台
+          <Menu.Item key="6" style={{ fontWeight: 'normal', fontSize: '14px' }}>
+            <IconFont type="icon-radio" />我的电台
           </Menu.Item>
-          <Menu.Item key="7" style={{fontWeight: 'normal', fontSize: '14px'}}>
-            <IconFont type="icon-collection"/>我的收藏
+          <Menu.Item key="7" style={{ fontWeight: 'normal', fontSize: '14px' }}>
+            <IconFont type="icon-collection" />我的收藏
           </Menu.Item>
-          {/* <SubMenu key="sub1" title="创建的歌单">
-            <Menu.Item key="8">创建的歌单</Menu.Item>
-            <Menu.Item key="9">收藏的歌单</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" title="收藏的歌单">
-            <Menu.Item key="10">创建的歌单</Menu.Item>
-            <Menu.Item key="11">收藏的歌单</Menu.Item>
-          </SubMenu> */}
         </Menu>
+        <Collapse ghost expandIconPosition="right" style={{marginTop: '10px'}}
+          expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+          >
+          <Panel header="创建的歌单" key="1">
+            <p className="collapse-item"><IconFont type="icon-like" />我喜欢的音乐</p>
+            <p className="collapse-item"><IconFont type="icon-songlist" />1</p>
+          </Panel>
+          <Panel header="创建的歌单" key="2">
+            <p className="collapse-item"><IconFont type="icon-songlist" />1</p>
+          </Panel>
+        </Collapse>
       </Fragment>
     )
   }
