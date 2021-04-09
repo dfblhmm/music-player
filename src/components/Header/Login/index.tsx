b19bd15822119ebe87bc63e7833192ec19050d96
dfblhmm <1449050328@qq.com>
2021-04-09 17:04:08 +0800

    完成扫码登录

--- src/components/Header/Login/index.tsx
+++ src/components/Header/Login/index.tsx
@@ -1,36 +1,92 @@
 import { Fragment, PureComponent } from 'react'
 import { Modal } from 'antd'
+import http from '@utils/http'
 import style from './index.module.scss'
 import ewmTip from '@assets/images/ewm_tip.png'
-import ewm from '@assets/images/ewm.png'
 
 interface LoginProps {
-  visible: boolean
+  loginSuccess: () => void
+  close: () => void
 }
-export default class Login extends PureComponent<LoginProps> {
+interface StateType {
+  ewmPic: string // 二维码Base64字符串
+  valid: boolean // 当前二维码是否还有效
+  type: number // 0表示二维码登录，1表示手机登录
+}
+export default class Login extends PureComponent<LoginProps, StateType> {
+  timer?: number
+  state = {
+    ewmPic: '', // 二维码图片
+    valid: false, // 二维码是否已有效
+    type: 0
+  }
+  componentDidMount() {
+    this.createEwm()
+  }
+  // 生成二维码
+  async createEwm() {
+    const keyData = await http('/login/qr/key', { timerstamp: Date.now() })
+    const key: string = keyData.data.unikey // 获取登录key
+    const res = await http('/login/qr/create', { key, qrimg: true, timerstamp: Date.now() })
+    const ewmPic: string = res.data.qrimg // 获取登录二维码
+    this.setState({ ewmPic, valid: true })
+    this.timer = window.setInterval(() => this.checkScan(key), 3000)
+  }
+  // 检测扫码状态
+  async checkScan(key: string) {
+    const res = await http('/login/qr/check', { key, timerstamp: Date.now() })
+    const code: number = res.code
+    switch (code) {
+      case 800: // 二维码过期
+        this.setState({ valid: false })
+        window.clearInterval(this.timer)
+        break
+      case 803: // 授权成功
+        window.clearInterval(this.timer)
+        this.props.loginSuccess()
+        break
+      default:
+        break
+    }
+  }
+  // 刷新二维码
+  refreshEwm = () => {
+    this.createEwm() // 生成新的二维码
+    this.setState({ valid: true })
+  }
   // 扫码登录
   ewmLogin(): JSX.Element {
+    const { ewmPic, valid } = this.state
     return (
       <div className={style['ewm-login-container']}>
         <h1>扫码登录</h1>
         <div className={style.img}>
-          <img src={ewmTip} alt="" />
-          <img src={ewm} alt="" />
+          <div className={style.tip}><img src={ewmTip} alt="" /></div>
+          <div className={style.ewm}>
+            <img src={ewmPic} alt="" />
+            <div className={style.mask} style={{display: !valid ? 'flex' : 'none'}}>
+              <h2>二维码已失效</h2><button onClick={this.refreshEwm}>点击刷新</button>
+            </div>
+          </div>
         </div>
         <div className={style.app}>使用<span>网易云音乐APP</span>扫码登录</div>
         <div className={style['other-loginType']}>选择其他登录方式&gt;</div>
       </div>
     )
   }
+  // 关闭对话框
+  close = () => {
+    window.clearInterval(this.timer)
+    this.props.close()
+  }
   render() {
-    const { visible } = this.props
     return (
-      <div>
-        <Modal visible={visible} mask={false} footer={null} 
-          centered width={352} >
+      <Fragment>
+        <Modal visible={true} mask={false} footer={null} 
+          centered width={352} onCancel={this.close} >
           {this.ewmLogin()}
         </Modal>
-      </div>
+      </Fragment>
     )
   }
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,32 @@
 import { PureComponent, Fragment } from 'react'
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
-import { Row, Col, Avatar } from 'antd'
+import { Row, Col, Avatar, message } from 'antd'
 import { LeftOutlined, RightOutlined, CaretDownOutlined } from '@ant-design/icons'
+import http from '@/utils/http'
 import Search from './Search'
 import Login from './Login'
 import logo from '@assets/images/logo.png'
 import style from './index.module.scss'
-class HeaderContainer extends PureComponent<RouteComponentProps> {
+interface StateType {
+  visible: boolean
+  accountInfo: { avatarUrl: string, nickname: string, userId: number },
+  loginStatus: boolean
+}
+class HeaderContainer extends PureComponent<RouteComponentProps, StateType> {
   state = {
-    visible: false
+    visible: false,
+    accountInfo: { avatarUrl: '', nickname: '', userId: 0 },
+    loginStatus: false
   }
+  async componentDidMount() {
+    const res = await http('/login/status', { timerstamp: Date.now() })
+    const profile: StateType['accountInfo'] | null = res.data.profile
+    // 未登录 
+    if (!profile) return message.warning('您还未登录~~~')
+    // 已登录
+    this.getUserInfo(profile)
+  }
+  
   // 前进按钮
   forward = () => {
     const { history: { goForward } } = this.props
@@ -20,8 +37,32 @@ class HeaderContainer extends PureComponent<RouteComponentProps> {
     const { history: { goBack } } = this.props
     goBack()
   }
+  // 获取登录信息
+  getUserInfo(profile: StateType['accountInfo']) {
+    const { avatarUrl, nickname, userId } = profile
+    this.setState({ 
+      accountInfo: { avatarUrl: avatarUrl + '?param=x28y28', nickname, userId },
+      loginStatus: true, visible: false 
+    })
+  }
+  // 登录成功，获取用户信息
+  loginSuccess = async () => {
+    const res = await http('/user/account')
+    const profile: StateType['accountInfo'] = res.profile
+    message.success(`${profile.nickname}，欢迎回来`)
+    this.getUserInfo(profile)
+  }
+  // 点击用户名
+  clickUserName = () => {
+    const { loginStatus } = this.state
+    if (!loginStatus) this.setState({ visible: true })
+  }
+  // 关闭对话框
+  closeModal = () => {
+    this.setState({ visible: false })
+  }
   render() {
-    const { visible } = this.state
+    const { visible, accountInfo: { avatarUrl, nickname } } = this.state
     return (
       <Fragment>
         <Row>
@@ -39,11 +80,11 @@ class HeaderContainer extends PureComponent<RouteComponentProps> {
           </Col>
           <Col span={8} offset={4} className={style.col}><Search /></Col>
           <Col span={8} className={style.col}>
-            <Avatar size={28} style={{cursor: 'pointer'}} />
-            <span className={style.username}  >
-              未登录<CaretDownOutlined style={{marginLeft: '3px'}} />
+            <Avatar size={28} style={{cursor: 'pointer'}} src={avatarUrl} />
+            <span className={style.username} onClick={this.clickUserName} >
+              {nickname ? nickname : '未登录'}<CaretDownOutlined style={{marginLeft: '3px'}} />
             </span>
-            <Login visible={visible} />
+            {visible ? <Login loginSuccess={this.loginSuccess} close={this.closeModal} /> : <></>}
           </Col>
         </Row>
       </Fragment>
