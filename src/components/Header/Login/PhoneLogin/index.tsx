ab1ec812eea2e549493599deaf28e689b7e53930
dfblhmm <1449050328@qq.com>
2021-04-10 14:51:56 +0800

    完成手机号登录

--- src/components/Header/Login/PhoneLogin/index.tsx
+++ src/components/Header/Login/PhoneLogin/index.tsx
@@ -1,5 +1,5 @@
 import { PureComponent } from 'react'
-import { Form, Input, Button } from 'antd'
+import { Input, Button, Image } from 'antd'
 import http from '@utils/http'
 import IconFont from '@components/IconFont'
 import ewm from '@assets/images/ewm.png'
@@ -8,30 +8,79 @@ import style from './index.module.scss'
 interface Props extends LoginSuccessProps {
   changeLoginType: (type: number) => void
 }
-export default class PhoneLogin extends PureComponent<Props> {
+interface State {
+  validateTip: string
+}
+export default class PhoneLogin extends PureComponent<Props, State> { 
+  phone!: Input | null
+  password!: Input | null
+  state = {
+    validateTip: ''
+  }
+  // 登录
+  login = async () => {
+    const phone = this.phone!.input.value
+    const password = this.password!.input.value
+    const phoneStatus = await this.validatePhone(phone)
+    if (!phoneStatus) return // 手机号检验未通过
+    // 手机号校验通过，开始校验密码
+    if (!this.validatePassword(password)) return
+    // 手机号和密码校验都通过，发起登录请求
+    const res = await http('/login/cellphone', { phone, password })
+    // 密码错误
+    if (res.code !== 200) return this.setState({ validateTip: '手机号或密码错误' })
+    // 登录成功
+    this.props.loginSuccess()
+  }
+  // 校验手机号
+  async validatePhone(phone: string) {
+    if (!phone.trim()) return this.setState({ validateTip: '手机号不能为空' })
+    const phoneRegExp = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
+    if (!phoneRegExp.test(phone)) return this.setState({ validateTip: '请输入合法的手机号' })
+    // 检验手机号是否注册
+    const res = await http('/cellphone/existence/check', { phone })
+    const exist = res.exist // 是否已注册，1代表已注册，-1代表未注册
+    if (exist !== 1) return this.setState({ validateTip: '该手机号尚未注册' })
+    return true
+  }
+  // 校验密码
+  validatePassword(password: string) {
+    if (!password.trim()) return this.setState({ validateTip: '密码不能为空' })
+    const passwordRegExp = /^(?![\d]+$)(?![a-zA-Z]+$)(?![^\da-zA-Z]+$).{8,20}$/
+    if (!passwordRegExp.test(password)) {
+      return this.setState({ validateTip: '密码8-20位，至少包含字母/数字/字符2种组合' })
+    }
+    return true
+  }
   render() {
     const { changeLoginType } = this.props
+    const { validateTip } = this.state
     return (
       <div className={style['phone-login-container']}>
         <div className={style.tip}>
           <div className={style.tipIcon} onClick={() => changeLoginType(0)}>
-            <img src={ewm} alt=""/>
+            <Image src={ewm} preview={false} />
           </div>
           <div className={style.tipText}>扫码登录更安全<span></span></div>
         </div>
         <div className={style['login-area']}>
-          <img src={phoneTip} alt=""/>
-          <Form name="normal_login" className={style['login-form']} initialValues={{ remember: true }}>
-            <Form.Item name="phone" rules={[{ required: true, message: '手机号不能为空' }]}>
-              <Input prefix={<IconFont type="icon-phone" />} placeholder="请输入手机号" />
-            </Form.Item>
-            <Form.Item name="password" rules={[{ required: true, message: '密码不能为空' }]}>
-              <Input prefix={<IconFont type="icon-password" />} type="password" placeholder="请输入密码" />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary">登录</Button>
-            </Form.Item>
-          </Form>
+          <Image src={phoneTip} preview={false}/>
+          {/* 登录区域 */}
+          <div className={style['login-form']}>
+            <div className={style['input-container']}>
+              <Input prefix={<IconFont type="icon-phone" />} 
+                allowClear placeholder="请输入手机号" bordered={false} ref={c => this.phone = c} />
+              <Input.Password prefix={<IconFont type="icon-password"/>} ref={c => this.password = c}
+                allowClear placeholder="请输入登录密码" bordered={false} />
+            </div>
+            {/* 表单校验提示 */}
+            <div className={style['validate']}>
+              <div className={style['error-tip']} style={{display: validateTip ? 'block' : 'none'}}>
+                <IconFont type="icon-warning" style={{marginRight: '2px', fontSize:'13px'}} />{validateTip}
+              </div>
+            </div>
+            <Button type="primary" className={style['login-btn']} onClick={this.login}>登录</Button>
+          </div>
         </div>
       </div>
     )
--- src/components/IconFont/index.ts
+++ src/components/IconFont/index.ts
@@ -1,4 +1,4 @@
 import { createFromIconfontCN } from '@ant-design/icons'
 export default createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_2124451_mso67p9k7fa.js',
+  scriptUrl: '//at.alicdn.com/t/font_2124451_5k9qgolu8a4.js',
 })
