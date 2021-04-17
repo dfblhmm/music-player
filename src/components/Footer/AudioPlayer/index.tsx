919453e6cc47eba810f759c4b82e5f90712ab455
dfblhmm <1449050328@qq.com>
2021-04-17 23:52:48 +0800

    更新进度

--- src/components/Footer/AudioPlayer/index.tsx
+++ src/components/Footer/AudioPlayer/index.tsx
@@ -1,6 +1,8 @@
 import { PureComponent } from 'react'
+import { Tooltip } from 'antd'
 import IconFont from '@components/IconFont'
 import style from './index.module.scss'
+const src = 'https://m701.music.126.net/20210417232317/91c81a6cc641b190f4b03f71e48aae42/jdyyaac/5558/5653/005a/b24d390feda9ed36089b70644163775d.m4a'
 interface IState {
   playMode: number // 播放模式
   playStatus: boolean // 播放状态
@@ -9,6 +11,7 @@ interface IProps {
 
 }
 export default class AudioPlayer extends PureComponent<IProps, IState> {
+  audio!: HTMLAudioElement | null
   state: IState = {
     playMode: 0,
     playStatus: false
@@ -29,23 +32,38 @@ export default class AudioPlayer extends PureComponent<IProps, IState> {
   }
   // 播放模式Icon
   Icon(type: string, title: string): JSX.Element {
+    const cssStyle = { 
+      fontSize: '12px', overflow: 'hidden', borderRadius: '5px', 
+      paddingLeft: '15px', paddingRight: '15px', letterSpacing: '.15em'
+    }
     return (
-      <IconFont type={type} className={style.icon} title={title} 
-        onClick={this.changeMode} />
+      <Tooltip placement="top" title={title} visible={true} overlayInnerStyle={cssStyle}>
+        <IconFont type={type} className={style.icon} title={title} 
+          onClick={this.changeMode} />
+      </Tooltip>
     )
   }
   // 改变播放模式
   changeMode = () => {
     this.setState(state => ({ playMode: state.playMode + 1 }))
   }
+  // 播放控制
+  playControl = () => {
+    // 播放/暂停音乐
+    const { playStatus } = this.state
+    if (playStatus) this.audio?.pause()
+    else this.audio?.play()
+    this.setState({ playStatus: !playStatus })
+  }
   render() {
     const { playMode, playStatus } = this.state
     return (
       <div className={style['audio-container']}>
+        <audio src={src} ref={c => this.audio = c}></audio>
         <div className={style['play-control']}>
           {this.changePlayMode(playMode)}
           <IconFont type="icon-prev-song" className={style.icon} title="上一首" />
-          <IconFont type={playStatus ? 'icon-pause' : 'icon-play'} 
+          <IconFont type={playStatus ? 'icon-pause' : 'icon-play'} onClick={this.playControl}
             className={style['play-icon']} title={playStatus ? '暂停' : '播放'} />
           <IconFont type="icon-next-song" className={style.icon} title="下一首" />
         </div>
